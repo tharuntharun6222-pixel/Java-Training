b36598fa3074fe355efb6920697fc4f300cda233
tharuntharun6222-pixel <tharuntharun6222@gmail.com>
2026-07-24 13:29:32 +0530

    Initial

--- Day1/Problem1.java
+++ Day1/Problem1.java
@@ -1,7 +1,17 @@
 package Day1;
-
+import java.util.Scanner;
 public class Problem1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+        if (ch >= 'A' && ch <= 'Z') {
+            System.out.println("Uppercase");
+        } else if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Lowercase");
+        } else {
+            System.out.println("Not an alphabet");
+        }
+        sc.close();
     }
 }
